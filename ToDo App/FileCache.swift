de264126b8f1e48362c2b3da5ab973ed127924be
Yegor Kolobaev <lroofer@icloud.com>
2024-06-21 22:15:13 +0300

    Added filecaching, modified a parser

---
+++ ToDo App/FileCache.swift
@@ -0,0 +1,61 @@
+//
+//  FileCache.swift
+//  ToDo App
+//
+//  Created by Егор Колобаев on 18.06.2024.
+//
+
+import Foundation
+
+class FileCache {
+    private(set) var items: Set<TodoItem>
+    
+    init? (contentsOf url: URL) {
+        guard let jsonData = try? Data(contentsOf: url), let jsonObject = try? JSONSerialization.jsonObject(with: jsonData) as? [TodoItem] else {
+            return nil
+        }
+        items = Set<TodoItem>()
+        for dataObject in jsonObject {
+            items.insert(dataObject)
+        }
+    }
+    
+    init () {
+        items = Set<TodoItem>()
+    }
+    
+    private func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+    
+    private func exportAllData() -> Data? {
+        var data = [Data]()
+        for element in items {
+            if let element = element.json as? Data {
+                data.append(element)
+            }
+        }
+        return try? JSONSerialization.data(withJSONObject: data)
+    }
+    
+    func addNew(task: TodoItem) {
+        items.insert(task)
+    }
+    
+    func deleteTask(with id: UUID) {
+        for element in items {
+            if element.id == id {
+                items.remove(element)
+                return
+            }
+        }
+    }
+    
+    func saveAllData(to file: String) throws {
+        let data = exportAllData() ?? Data()
+        let filename = getDocumentsDirectory().appendingPathComponent(file)
+        try data.write(to: filename)
+    }
+    
+}
---
+++ ToDo App/JsonParsing.swift
@@ -0,0 +1,60 @@
+//
+//  JsonParsing.swift
+//  ToDo App
+//
+//  Created by Егор Колобаев on 19.06.2024.
+//
+
+import Foundation
+
+extension TodoItem {
+    enum TodoItemStoredFields: String {
+        case id = "id"
+        case text = "text"
+        case priority = "priority"
+        case deadline = "deadline"
+        case completed = "completed"
+    }
+    
+    static func convertToData(json: Any) -> Data? {
+        if let json = json as? String {
+            return Data(json.utf8)
+        }
+        return json as? Data
+    }
+    
+    static func parse(json: Any) -> TodoItem? {
+        guard let jsonData = convertToData(json: json),
+                let jsonObject = try? JSONSerialization.jsonObject(with: jsonData) as? [String: Any] else {
+            return nil
+        }
+        let getValue = { (identifier: TodoItemStoredFields) in jsonObject[identifier.rawValue]
+        }
+        
+        let id = (getValue(.id) as? UUID) ?? UUID()
+        guard let text = getValue(.text) as? String else {
+            return nil
+        }
+        let priority = (getValue(.priority) as? PriorityChoices) ?? .ordinary
+        let deadline = getValue(.deadline) as? Date
+        guard let completed = getValue(.completed) as? Bool else {
+            return nil
+        }
+        return TodoItem(id: id, text: text, priority: priority, deadline: deadline, completed: completed, creationDate: Date.now, lastChangeDate: nil)
+    }
+    
+    var json: Any {
+        var object = [String: Any]()
+        let setValue = {(identifier: TodoItemStoredFields, value: Any) in object[identifier.rawValue] = value}
+        setValue(.id, id)
+        setValue(.text, text)
+        if priority != .ordinary {
+            setValue(.priority, priority)
+        }
+        if deadline != nil {
+            setValue(.deadline, deadline!)
+        }
+        setValue(.completed, completed)
+        return (try? JSONSerialization.data(withJSONObject: object)) ?? Data()
+    }
+}
