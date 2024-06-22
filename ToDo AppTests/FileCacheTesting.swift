65c46134e362936f715726b480277e7d5c409acd
Yegor Kolobaev <lroofer@icloud.com>
2024-06-22 04:47:38 +0300

    pre-final modifications

---
+++ ToDo AppTests/FileCacheTesting.swift
@@ -0,0 +1,16 @@
+//
+//  FileCacheTesting.swift
+//  ToDo AppTests
+//
+//  Created by Егор Колобаев on 22.06.2024.
+//
+
+import Testing
+
+struct FileCacheTesting {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
