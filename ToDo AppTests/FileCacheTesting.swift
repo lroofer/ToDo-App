1e6bef8abae462ec969d42de709f0174d2b45f52
Yegor Kolobaev <lroofer@icloud.com>
2024-06-23 13:41:11 +0300

    final changes

--- ToDo AppTests/FileCacheTesting.swift
+++ ToDo AppTests/FileCacheTesting.swift
@@ -6,28 +6,29 @@
 //
 
 import Foundation
-import Testing
+import XCTest
 @testable import ToDo_App
 
-struct FileCacheTesting {
-
-    @Test func testFetching() async throws {
+final class FileCacheTesting: XCTestCase {
+    
+    func testFetching() async throws {
         let fileCache = FileCache()
-        fileCache.addNew(task: TodoItem(id: nil, text: "Task 1", priority: .low, deadline: nil, completed: false, creationDate: .now, lastChangeDate: nil))
+        let task1 = TodoItem(id: nil, text: "Task 1", priority: .low, deadline: nil, completed: false, creationDate: .now, lastChangeDate: nil)
+        fileCache.addNew(task: task1)
         fileCache.addNew(task: TodoItem(id: nil, text: "Task 2", priority: .high, deadline: .now, completed: false, creationDate: .now, lastChangeDate: .now))
-        fileCache.addNew(task: fileCache.tasks[fileCache.tasks.startIndex])
-        #expect(fileCache.tasks.count == 2)
-        fileCache.deleteTask(with: fileCache.tasks[fileCache.tasks.startIndex].id)
-        #expect(fileCache.tasks.count == 1)
-        fileCache.addNew(task: TodoItem(id: nil, text: "Task 1", priority: .low, deadline: nil, completed: false, creationDate: .now, lastChangeDate: nil))
-        #expect(fileCache.tasks.count == 2)
+        fileCache.addNew(task: task1)
+        XCTAssert(fileCache.tasks.count == 2)
+        _ = fileCache.deleteTask(with: task1.id)
+        XCTAssert(fileCache.tasks.count == 1)
+        fileCache.addNew(task: task1)
+        XCTAssert(fileCache.tasks.count == 2)
         let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
         
         let filename = paths[0].appendingPathComponent("test_2.csv")
         
         try fileCache.saveAllData(to: filename)
-        let fileCache_exported = FileCache(contentsOf: filename)
-        #expect(fileCache.tasks.count == 2)
+        _ = FileCache(contentsOf: filename)
+        XCTAssert(fileCache.tasks.count == 2)
     }
-
+    
 }
