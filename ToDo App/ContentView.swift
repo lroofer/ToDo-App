18ba088e9d237bd48a3b0469a8955e9f00766b3f
Yegor Kolobaev <lroofer@icloud.com>
2024-06-25 02:25:30 +0300

    Connected TodoItemView to ContentView + added the first version of the design

--- ToDo App/ContentView.swift
+++ ToDo App/ContentView.swift
@@ -8,31 +8,76 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showTask = false
-    @State private var selectedView: TodoItemView? = nil
     @State var tasks: [TodoItemView]
+    @State var addNewShow = false
+    @State var showCompleted = false
     var body: some View {
         NavigationStack {
-            ScrollView {
-                LazyVStack (alignment: .leading) {
-                    ForEach($tasks, id: \.attachedItem.id) { task in
-                        HStack {
-                            Button("Open") {
-                                selectedView = task.wrappedValue
-                                showTask.toggle()
+            List{
+                Section(header: HStack {
+                    Text("Completed - none")
+                    Spacer()
+                    Button(showCompleted ? "Hide" : "Show") {
+                        showCompleted.toggle()
+                    }
+                }
+                    .font(.callout)
+                    .textCase(.none)
+                ) {
+                    ForEach(0..<tasks.count, id:\.self) { id in
+                        if !tasks[id].attachedItem.done || showCompleted {
+                            NavigationLink(value: id) {
+                                HStack {
+                                    Button(action: {
+                                        
+                                    }, label: {
+                                        if tasks[id].attachedItem.done {
+                                            Image(systemName: "circle.inset.filled")
+                                        } else {
+                                            Image(systemName: "circle")
+                                        }
+                                    })
+                                    .buttonBorderShape(.roundedRectangle)
+                                    .buttonStyle(.plain)
+                                    VStack (alignment: .leading) {
+                                        Text(tasks[id].attachedItem.text)
+                                            .lineLimit(1)
+                                            .font(.system(size: 17))
+                                        if tasks[id].attachedItem.deadline != nil {
+                                            HStack {
+                                                Image(systemName: "calendar")
+                                                Text(tasks[id].attachedItem.deadline!.formatted(date: .abbreviated, time: .omitted))
+                                            }
+                                            .font(.system(size: 13))
+                                            .foregroundColor(.accentColor)
+                                        }
+                                    }
+                                    .padding(5)
+                                }
                             }
-                            Text("item: \(task.wrappedValue.attachedItem.id)")
                         }
                     }
-                    .padding()
                 }
             }
+            .navigationDestination(for: Int.self, destination: { id in
+                tasks[id]
+            })
             .navigationTitle("My tasks")
-            .sheet(isPresented: $showTask) {
-                if selectedView != nil {
-                    selectedView
+        }
+        .overlay(alignment: .bottom) {
+            if (!addNewShow) {
+                Button("+") {
+                    addNewShow.toggle()
                 }
+                .frame(width: 44, height: 44)
+                .font(.largeTitle)
+                .foregroundStyle(.white)
+                .background(.blue)
+                .clipShape(Circle())
             }
+        }
+        .sheet(isPresented: $addNewShow) {
+            TodoItemView()
         }
     }
 }
