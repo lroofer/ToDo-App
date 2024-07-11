a203771c3e11e5b65b20eb30da8b665ec7011cfb
Yegor Kolobaev <lroofer@icloud.com>
2024-07-11 09:06:58 +0300

    fixed uikit pass data (works with split view as well)

--- ToDo App/CalendarView/CalendarRepresentable.swift
+++ ToDo App/CalendarView/CalendarRepresentable.swift
@@ -10,15 +10,18 @@ import SwiftUI
 
 struct CalendarRepresentable: UIViewControllerRepresentable {
     @Binding var showTaskView: Bool
+    @Binding var selectedTask: TodoItem?
     var model: Todos
     func makeUIViewController(context: Context) -> UINavigationController {
-        return UINavigationController(rootViewController: CalendarView(model: model, showTaskView: $showTaskView.wrappedValue))
+        return UINavigationController(rootViewController: CalendarView(model: model, updateState: { status, value in
+                showTaskView = status
+            selectedTask = value
+        }))
     }
     
     func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
         if let calendar = uiViewController.viewControllers.first as? CalendarView {
             calendar.model = model
-            showTaskView = calendar.showTaskView
         }
     }
 }
